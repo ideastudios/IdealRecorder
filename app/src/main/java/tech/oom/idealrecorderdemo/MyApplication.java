8831fdf9a3aa199389ff4170195ad83e66aca542
ideastudios <815106625@qq.com>
2019-02-26 18:53:56 +0800

    modify init way

--- app/src/main/java/tech/oom/idealrecorderdemo/MyApplication.java
+++ app/src/main/java/tech/oom/idealrecorderdemo/MyApplication.java
@@ -9,6 +9,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        IdealRecorder.init(this);
+        IdealRecorder.getInstance().init(this);
     }
 }

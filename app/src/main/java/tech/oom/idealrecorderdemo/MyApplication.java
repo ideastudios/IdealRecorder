42a2d276cf785c5c2b8b211e6258163329d3ed1b
ideastudios <815106625@qq.com>
2019-02-26 17:56:33 +0800

    add callback on worker thread

--- app/src/main/java/tech/oom/idealrecorderdemo/MyApplication.java
+++ app/src/main/java/tech/oom/idealrecorderdemo/MyApplication.java
@@ -9,6 +9,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        IdealRecorder.getInstance().init(this);
+        IdealRecorder.init(this);
     }
 }
